c0b3f9953bfaacbbc090010ca50af71eec3d556d
Suyash Raj <suyash10581108@gmail.com>
2024-06-17 20:53:59 -0700

    Added git config global vars

--- git/install.sh
+++ git/install.sh
@@ -5,3 +5,7 @@
 
 # Linux
 sudo apt install git
+
+# Add user name and email to git global config
+git config --global user.name "Suyash Raj"
+git config --global user.email "suyash10581108@gmail.com"
